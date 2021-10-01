20ad81789589bf52bf936f4cdc5f33c7f363de98
Rituraj Gupta <rituraj.iter@gmail.com>
2021-10-01 10:28:10 +0530

    Added Files

---
+++ README.md
@@ -0,0 +1,5 @@
+# Contributors
+
+### 𝐇𝐞𝐥𝐥𝐨 𝐭𝐡𝐞𝐫𝐞, 𝐟𝐞𝐥𝐥𝐨𝐰 <𝚌𝚘𝚍𝚎𝚛𝚜/>! <img src="https://raw.githubusercontent.com/ABSphreak/ABSphreak/master/gifs/Hi.gif" width="50px">
+
+This website is been made to show the contribution of participants in Hacktoberfest 2021 for the repository Hacktoberfest-2021 of HacOkars organisation.
---
+++ src/App.js
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import Card from "./Card";
+
+function App() {
+  const [allUsers, setAllUsers] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      let userData;
+      try {
+        const response = await fetch("https://api.github.com/repos/HacOkars/Hacktoberfest-2021/contributors");
+        userData = await response.json();
+      } catch (error) {
+        console.log(error);
+        userData = [];
+      }
+      setAllUsers(userData);
+      setUsers(userData);
+    })();
+  }, []);
+
+  const filterCards = event => {
+    const value = event.target.value.toLowerCase();
+    const filteredUsers = allUsers.filter(user => (`${user.login}`.toLowerCase().includes(value)));
+    setUsers(filteredUsers);
+  }
+
+  return (
+    <div className="App">
+      <a href="https://hacktoberfest.digitalocean.com/"><img src="https://hacktoberfest.digitalocean.com/_nuxt/img/logo-hacktoberfest-full.f42e3b1.svg" id="hacktober"></img></a>
+      <h1>Contributors Cards</h1>
+      <input className="search-box" onInput={filterCards} placeholder="Search..." />
+      <div className="cards-container">
+
+        {users.map((user, index) => (
+          <Card key={index} userData={user} />
+        ))}
+      </div>
+      <img src="https://github.githubassets.com/images/spinners/octocat-spinner-64.gif"></img>
+      <footer>
+        &copy; Designed by: <a href="http://riturajgupta.rf.gd/">&nbsp; Rituraj Gupta</a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
